4722a29ece99b53260e178322fd45c4ee410b0df
HaniaAmayem <68034764+HaniaAmayem@users.noreply.github.com>
2020-09-08 01:18:12 +0200

    Update SpeakGoodbye.js

--- Module4-solution/SpeakGoodbye.js
+++ Module4-solution/SpeakGoodbye.js
@@ -3,7 +3,7 @@
   var byeSpeaker = {};	
   var speakWord = "Good Bye";
 
-  byespeaker.speak = function (name){
+  byeSpeaker.speak = function (name){
     console.log(speakWord + " " + name);
   }
   window.byeSpeaker = byeSpeaker;
