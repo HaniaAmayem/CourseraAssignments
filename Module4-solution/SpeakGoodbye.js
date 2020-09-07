d803e84c5874a17cf672bfa49530f8719e2286d8
HaniaAmayem <68034764+HaniaAmayem@users.noreply.github.com>
2020-09-08 01:38:37 +0200

    Update and rename SpeakGoodBye.js to SpeakGoodbye.js

--- Module4-solution/SpeakGoodbye.js
+++ Module4-solution/SpeakGoodbye.js
@@ -1,10 +1,10 @@
 ( function (window){
 
-  //var byeSpeaker = {};
+  var byeSpeaker = {};
   var speakWord = "Good Bye";
 
- // byeSpeaker.speak = function (name){
- 	var byeSpeaker = function (name){
+  byeSpeaker.speak = function (name){
+ 	//var byeSpeaker = function (name){
     console.log(speakWord + " " + name);
   }
   window.byeSpeaker = byeSpeaker;
